7cd8be3458c81707b174d9b15b286a5364e7463b
shambel-amare <shambelamare2006@gmail.com>
2022-04-07 20:56:08 +0000

    task 6 up2

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ char c;
 long l;
 float f;
 long long ll;
-printf("The size of a char: ", (unsigned long)sizeof(c));
-printf("The size of an int: ", (unsigned long)sizeof(i));
-printf("The size of a long int: ", (unsigned long)sizeof(l));
-printf("The size of a long long int: ", (unsigned long)sizeof(ll));
-printf("The size of a float: ", (unsigned long)sizeof(f));
+printf("The size of a char: %lu \n", (unsigned long)sizeof(c));
+printf("The size of an int: %lu \n", (unsigned long)sizeof(i));
+printf("The size of a long int: %lu \n", (unsigned long)sizeof(l));
+printf("The size of a long long int: %lu \n", (unsigned long)sizeof(ll));
+printf("The size of a float: %lu \n", (unsigned long)sizeof(f));
 return (0);
 }
