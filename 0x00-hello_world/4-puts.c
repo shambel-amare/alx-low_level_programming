0d213efc515879bbe94b87c56f8ffd7fbfe0443b
shambel-amare <shambelamare2006@gmail.com>
2022-04-07 20:30:25 +0000

    task 4 up3

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 int main(void)
+/**
+*this function prints the string str2
+*/
 {
-char str1[60] = "\"Programming is like building a multilingual puzzle\n\"";
+char str1[60] = "\"Programming is like building a multilingual puzzle\n";
 char str2[60];
 strcpy(str2, str1);
 puts(str2);
