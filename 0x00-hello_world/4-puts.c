474d04b68ca1b9d27536aea46eaf6f8de33207ba
shambel-amare <shambelamare2006@gmail.com>
2022-04-07 20:22:46 +0000

    task 4 up1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <string.h>
-
-int main(void){
-    char str1[60]="Programming is like building a multilingual puzzle\n";
-    char str2[60];
-    strcpy(str2,str1);
-    puts(str2);
-    return (0);
+int main(void)
+{
+char str1[60] = "\"Programming is like building a multilingual puzzle\n\"";
+char str2[60];
+strcpy(str2, str1);
+puts(str2);
+return (0);
 }
