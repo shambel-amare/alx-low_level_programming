399f73884f99e0e59016e77f2b5ece0ed9a3217e
shambel-amare <shambelamare2006@gmail.com>
2022-04-08 22:20:16 +0000

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*Return: Always 0 (success)
+*/
+int main(void)
+{
+int c;
+for (c = 0; c < 10; c++)
+{
+printf("%d", c);
+}
+putchar('\n');
+return (0);
+}
