0f4e4a3f49d96ceb2997194e7e13db12c1c8b989
shambel-amare <shambelamare2006@gmail.com>
2022-04-12 10:22:11 +0000

    A function to print the sign os a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*print_sign prints the sign ofa number
+* main - check the code
+* print_sign function resides in main.h
+*Return: Always 0 (success)
+*/
+
+int main(void)
+{
+int r;
+
+r = print_sign(98);
+_putchar(',');
+_putchar(' ');
+_putchar(r + '0');
+_putchar('\n');
+r = print_sign(0);
+_putchar(',');
+_putchar(' ');
+_putchar(r + '0');
+_putchar('\n');
+r = print_sign(0xff);
+_putchar(',');
+_putchar(' ');
+_putchar(r + '0');
+_putchar('\n');
+r = print_sign(-1);
+_putchar(',');
+_putchar(' ');
+_putchar(r + '0');
+_putchar('\n');
+return(0);
+}
