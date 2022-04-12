3d30ad8c89f93644f5847c9cb5fc20ca05e59e22
shambel-amare <shambelamare2006@gmail.com>
2022-04-12 08:43:22 +0000

    simple while loop to print alphabet 10 times!

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* main - Entry point
+*Return: Always 0 (success)
+*/
+
+int main(void)
+{
+int i = 0;
+while (i < 10)
+{
+print_alphabet();
+_putchar('\n');
+i++;
+}
+return (0);
+}
