b4c79c97e267c325d6a712ff514c38d3eb48e54b
shambel-amare <shambelamare2006@gmail.com>
2022-04-14 20:47:23 +0000

    task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,18 +8,29 @@ int main(void)
 int c, j, k, l;
 for (c = 0; c < 10; c++)
 {
-for (j = 0; j < 10; j++)
+for (i = 0; i < 10; i++)
 {
 for (k = 0; k < 10; k++)
 {
-for (l = 0; l < 10; l++)
+for (j = 0; j < 10; j++)
+{
+if (((k + j) > (c + i) &&  k >= c) || c < k)
 {
 putchar(c + '0');
-putchar(j + '0');
+putchar(i + '0');
 putchar(' ');
-putchar(k + '0');
-putchar(l + '0');
+putchar(k+ '0');
+putchar(j + '0');
+}
+else if (c + i + k + j == 227 && c == 57)
+{
+break;
+}
+else
+{
 putchar(',');
+putchar(' ');
+}
 }
 }
 }
