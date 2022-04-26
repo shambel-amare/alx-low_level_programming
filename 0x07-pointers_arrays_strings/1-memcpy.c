b28861ac51b21c61c2a0fd76c32f21155e54bb70
shambel-amare <shambelamare2006@gmail.com>
2022-04-26 18:49:15 +0000

    memcopy function

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,9 +11,10 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-unsigned int i = 0;
-for (i; i < n; i++)
+unsigned int i;
+for (i = 0; i < n; i++)
 {
-dest[i] = src[i];
+*(dest + i) = *(src + i);
 }
+return (dest);
 }
