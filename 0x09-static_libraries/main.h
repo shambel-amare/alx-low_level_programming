38fd2c7682b67fd7ab64c9acba2150fd353d43ed
shambel-amare <shambelamare2006@gmail.com>
2022-04-29 21:20:20 +0000

    Project 0x009
    correcting shell error

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,21 +1,20 @@
-#ifndef _FUNCTIONS_H
-#define _FUNCTIONS_H
+#ifndef MAIN_H
+#define MAIN_H
 #include <stdio.h>
-int _putchar(char c);
+
 int _islower(int c);
 int _isalpha(int c);
+int _putchar(char c);
 int _abs(int n);
 int _isupper(int c);
 int _isdigit(int c);
 int _strlen(char *s);
 void _puts(char *s);
 char *_strcpy(char *dest, char *src);
-void positive_or_negative(int i);
 int _atoi(char *s);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
-void print_remaining_days(int month, int day, int year);
 int _strcmp(char *s1, char *s2);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
@@ -23,4 +22,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-#endif
+
+#endif /*MAIN_H*/
