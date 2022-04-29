2ee29c8d350dee44ca83259e663680e4ad4ab606
shambel-amare <shambelamare2006@gmail.com>
2022-04-29 22:17:12 +0000

    argc argv first task

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+ * main - Entry point
+ *@argc: argument count
+ *@argv: array of input arguments
+ *Return: Always 0 (success)
+ */
 int main(int argc, char **argv)
 {
-    printf("%s\n", *argv);
+printf("%s\n", *argv);
 }
