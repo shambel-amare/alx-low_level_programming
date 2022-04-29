dae45d5d8388351a9ac4ad40760bda361d200922
shambel-amare <shambelamare2006@gmail.com>
2022-04-29 22:28:12 +0000

    print number of input arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *@argc: argument count
+ *@argv: array of input arguments
+ *Return: Always 0 (success)
+ */
+int main(int argc, char **argv)
+{
+    printf("%d\n", argc-1);
+}
