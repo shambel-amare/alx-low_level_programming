142760ba7532db8d5ef92ec741778b162570d097
shambel-amare <shambelamare2006@gmail.com>
2022-04-29 20:49:03 +0000

    Project 0x009
    Introduction to creating static libraries

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,6 @@
 #ifndef _FUNCTIONS_H
 #define _FUNCTIONS_H
+#include <stdio.h>
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -9,10 +10,12 @@ int _isdigit(int c);
 int _strlen(char *s);
 void _puts(char *s);
 char *_strcpy(char *dest, char *src);
+void positive_or_negative(int i);
 int _atoi(char *s);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
+void print_remaining_days(int month, int day, int year);
 int _strcmp(char *s1, char *s2);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
